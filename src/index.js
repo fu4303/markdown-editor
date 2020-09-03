1fb61fa5fe439d28618e6cc9eecf3556ec2bba23
Justin Juno <hello@justinjuno.dev>
2020-09-03 14:42:23 -0500

    add layout wrapper and proptypes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { Router } from "@reach/router"
+import Layout from "./components/layout"
 import Home from "./pages/Home"
 import Create from "./pages/Create"
 import NotFound from "./pages/NotFound"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Home path="/" />
-      <Create path="/create" />
-      <NotFound path="*" />
-    </Router>
+    <Layout>
+      <Router>
+        <Home path="/" />
+        <Create path="/create" />
+        <NotFound path="*" />
+      </Router>
+    </Layout>
   </React.StrictMode>,
   document.getElementById("root")
 )
